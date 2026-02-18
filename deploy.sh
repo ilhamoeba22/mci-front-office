92c94522ae4e82f94601ee87f0a00a070f111662
unknown <Muhamamd Ilham Pratama>
2026-02-18 10:14:02 +0700

    Deploy: Use local build strategy

--- deploy.sh
+++ deploy.sh
@@ -9,18 +9,13 @@ echo "--- Memulai Update Produksi ---"
 echo "[1/3] Menarik kode terbaru dari GitHub..."
 git pull origin main
 
-# 2. Build Assets (Vite)
-echo "[2/4] Menginstal dependensi dan membuat build asset..."
-npm install
-npm run build
-
-# 3. Optimalisasi Laravel
-echo "[3/4] Membersihkan dan membuat cache baru..."
+# 2. Optimalisasi Laravel
+echo "[2/3] Membersihkan dan membuat cache baru..."
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 php artisan migrate --force
 
-# 4. Selesai
-echo "[4/4] Selesai! Website sudah terupdate."
+# 3. Selesai
+echo "[3/3] Selesai! Website sudah terupdate."
 echo "--- Update Berhasil ---"
